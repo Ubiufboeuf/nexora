c25d7b832d7411c4f9d2ca971346ef630fc17ffe
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-07-13 20:12:13 -0300

    feat: añadir constantes "globales"

---
+++ app/lib/constants.ts
@@ -0,0 +1,7 @@
+const SERVER_URL = import.meta.env.VITE_SERVER_URL
+
+export const THUMBNAIL_ENDPOINT = `${SERVER_URL}/poster`
+export const VIDEO_STREAMING_ENDPOINT = `${SERVER_URL}/video`
+
+export const VIDEO_ENDPOINT = `${SERVER_URL}/data/video`
+export const VIDEOS_ENDPOINT = `${SERVER_URL}/data/videos`
