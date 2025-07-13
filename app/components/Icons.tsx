7055382889d29cb8be7f0b61dfb170e00b630bd8
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-07-13 20:14:11 -0300

    feat: añadir lista de iconos

---
+++ app/components/Icons.tsx
@@ -0,0 +1,268 @@
+export const IconClose = ({ strokeWidth = '1.5' }: { strokeWidth?: string }) => (
+  <svg
+    width='24'
+    height='24'
+    viewBox='0 0 24 24'
+    fill='none'
+    stroke='currentColor'
+    strokeWidth={strokeWidth}
+    xmlns='http://www.w3.org/2000/svg'
+    className='h-full w-full pointer-events-none object-contain'
+  >
+    <path d='M12.0005 13.0543L6.9275 18.1275C6.789 18.2658 6.61492 18.3367 6.40525 18.34C6.19575 18.3432 6.0185 18.2723 5.8735 18.1275C5.72867 17.9825 5.65625 17.8068 5.65625 17.6005C5.65625 17.3942 5.72867 17.2185 5.8735 17.0735L10.9468 12.0005L5.8735 6.9275C5.73517 6.789 5.66433 6.61492 5.661 6.40525C5.65783 6.19575 5.72867 6.0185 5.8735 5.8735C6.0185 5.72867 6.19417 5.65625 6.4005 5.65625C6.60683 5.65625 6.7825 5.72867 6.9275 5.8735L12.0005 10.9468L17.0735 5.8735C17.212 5.73517 17.3861 5.66433 17.5958 5.661C17.8053 5.65783 17.9825 5.72867 18.1275 5.8735C18.2723 6.0185 18.3448 6.19417 18.3448 6.4005C18.3448 6.60683 18.2723 6.7825 18.1275 6.9275L13.0543 12.0005L18.1275 17.0735C18.2658 17.212 18.3367 17.3861 18.34 17.5958C18.3432 17.8053 18.2723 17.9825 18.1275 18.1275C17.9825 18.2723 17.8068 18.3448 17.6005 18.3448C17.3942 18.3448 17.2185 18.2723 17.0735 18.1275L12.0005 13.0543Z' fill='currentColor' />
+  </svg>
+)
+
+export const IconMenu = ({ strokeWidth = '1.5' }: { strokeWidth?: string }) => (
+  <svg
+    width='24'
+    height='24'
+    viewBox='0 0 24 24'
+    fill='none'
+    stroke='currentColor'
+    strokeWidth={strokeWidth}
+    xmlns='http://www.w3.org/2000/svg'
+    className='h-full w-full pointer-events-none object-contain'
+  >
+    <path d='M4.25 17.6342C4.0375 17.6342 3.85942 17.5623 3.71575 17.4185C3.57192 17.2748 3.5 17.0967 3.5 16.884C3.5 16.6715 3.57192 16.4934 3.71575 16.3497C3.85942 16.2062 4.0375 16.1345 4.25 16.1345H19.75C19.9625 16.1345 20.1406 16.2063 20.2843 16.35C20.4281 16.4938 20.5 16.6721 20.5 16.8847C20.5 17.0972 20.4281 17.2753 20.2843 17.419C20.1406 17.5625 19.9625 17.6342 19.75 17.6342H4.25ZM4.25 12.7497C4.0375 12.7497 3.85942 12.6778 3.71575 12.534C3.57192 12.3902 3.5 12.212 3.5 11.9995C3.5 11.7868 3.57192 11.6087 3.71575 11.4652C3.85942 11.3216 4.0375 11.2497 4.25 11.2497H19.75C19.9625 11.2497 20.1406 11.3217 20.2843 11.4655C20.4281 11.6093 20.5 11.7875 20.5 12C20.5 12.2127 20.4281 12.3907 20.2843 12.5342C20.1406 12.6779 19.9625 12.7497 19.75 12.7497H4.25ZM4.25 7.86498C4.0375 7.86498 3.85942 7.79315 3.71575 7.64948C3.57192 7.50565 3.5 7.3274 3.5 7.11473C3.5 6.90223 3.57192 6.72415 3.71575 6.58048C3.85942 6.43698 4.0375 6.36523 4.25 6.36523H19.75C19.9625 6.36523 20.1406 6.43715 20.2843 6.58098C20.4281 6.72465 20.5 6.90282 20.5 7.11548C20.5 7.32798 20.4281 7.50607 20.2843 7.64973C20.1406 7.79323 19.9625 7.86498 19.75 7.86498H4.25Z' fill='currentColor' />
+  </svg>
+)
+
+export const IconGlobalSearch = ({ strokeWidth = '1.5' }: { strokeWidth?: string }) => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    width='24'
+    height='24'
+    viewBox='0 0 24 24'
+    fill='none'
+    stroke='currentColor'
+    strokeWidth={strokeWidth}
+    strokeLinecap='round'
+    strokeLinejoin='round'
+    className='h-full w-full pointer-events-none object-contain'
+  >
+    <path d='M21 12a9 9 0 1 0 -9 9' />
+    <path d='M3.6 9h16.8' />
+    <path d='M3.6 15h7.9' />
+    <path d='M11.5 3a17 17 0 0 0 0 18' />
+    <path d='M12.5 3a16.984 16.984 0 0 1 2.574 8.62' />
+    <path d='M18 18m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0' />
+    <path d='M20.2 20.2l1.8 1.8' />
+  </svg>
+)
+
+export const IconSettings = ({ strokeWidth = '1.5' }: { strokeWidth?: string }) => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    width='24'
+    height='24'
+    viewBox='0 0 24 24'
+    fill='none'
+    stroke='currentColor'
+    strokeWidth={strokeWidth}
+    strokeLinecap='round'
+    strokeLinejoin='round'
+    className='h-full w-full pointer-events-none object-contain'
+  >
+    <path d='M10.325 4.317c.426 -1.756 2.924 -1.756 3.35 0a1.724 1.724 0 0 0 2.573 1.066c1.543 -.94 3.31 .826 2.37 2.37a1.724 1.724 0 0 0 1.065 2.572c1.756 .426 1.756 2.924 0 3.35a1.724 1.724 0 0 0 -1.066 2.573c.94 1.543 -.826 3.31 -2.37 2.37a1.724 1.724 0 0 0 -2.572 1.065c-.426 1.756 -2.924 1.756 -3.35 0a1.724 1.724 0 0 0 -2.573 -1.066c-1.543 .94 -3.31 -.826 -2.37 -2.37a1.724 1.724 0 0 0 -1.065 -2.572c-1.756 -.426 -1.756 -2.924 0 -3.35a1.724 1.724 0 0 0 1.066 -2.573c-.94 -1.543 .826 -3.31 2.37 -2.37c1 .608 2.296 .07 2.572 -1.065z' />
+    <path d='M9 12a3 3 0 1 0 6 0a3 3 0 0 0 -6 0' />
+  </svg>
+)
+
+// = dos estados =
+
+export const IconSearch = ({ strokeWidth = '1.5' }: { strokeWidth?: string }) => (
+  <svg
+    width='24'
+    height='24'
+    viewBox='0 0 24 24'
+    fill='none'
+    stroke='currentColor'
+    strokeWidth={strokeWidth}
+    xmlns='http://www.w3.org/2000/svg'
+    className='h-full w-full pointer-events-none object-contain'
+  >
+    <path d='M9.51955 15.6153C7.81188 15.6153 6.36571 15.023 5.18105 13.8385C3.99655 12.6538 3.4043 11.2077 3.4043 9.50002C3.4043 7.79235 3.99655 6.34618 5.18105 5.16152C6.36571 3.97702 7.81188 3.38477 9.51955 3.38477C11.2272 3.38477 12.6734 3.97702 13.858 5.16152C15.0425 6.34618 15.6348 7.79235 15.6348 9.50002C15.6348 10.2142 15.515 10.8963 15.2753 11.5463C15.0355 12.1963 14.7155 12.7616 14.3155 13.2423L20.0695 18.9963C20.208 19.1346 20.2789 19.3086 20.282 19.5183C20.2852 19.7279 20.2144 19.9052 20.0695 20.05C19.9247 20.1948 19.749 20.2673 19.5425 20.2673C19.3362 20.2673 19.1606 20.1948 19.0158 20.05L13.2618 14.296C12.7618 14.7088 12.1868 15.0319 11.5368 15.2653C10.8868 15.4986 10.2144 15.6153 9.51955 15.6153ZM9.51955 14.1155C10.808 14.1155 11.8994 13.6683 12.7935 12.774C13.6879 11.8798 14.135 10.7885 14.135 9.50002C14.135 8.21152 13.6879 7.12018 12.7935 6.22601C11.8994 5.33168 10.808 4.88452 9.51955 4.88452C8.23105 4.88452 7.13971 5.33168 6.24555 6.22601C5.35121 7.12018 4.90405 8.21152 4.90405 9.50002C4.90405 10.7885 5.35121 11.8798 6.24555 12.774C7.13971 13.6683 8.23105 14.1155 9.51955 14.1155Z' fill='currentColor' />
+  </svg>
+)
+
+export const IconHome = ({ strokeWidth = '1.5' }: { strokeWidth?: string }) => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    width='24'
+    height='24'
+    viewBox='0 0 24 24'
+    fill='none'
+    stroke='currentColor'
+    strokeWidth={strokeWidth}
+    strokeLinecap='round'
+    strokeLinejoin='round'
+    className='h-full w-full pointer-events-none object-contain'
+  >
+    <path d='m3 9 9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z' />
+    <polyline points='9 22 9 12 15 12 15 22' />
+  </svg>
+)
+
+export const IconRecent = ({ strokeWidth = '1.5' }: { strokeWidth?: string }) => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    width='24'
+    height='24'
+    viewBox='0 0 24 24'
+    fill='none'
+    stroke='currentColor'
+    strokeWidth={strokeWidth}
+    strokeLinecap='round'
+    strokeLinejoin='round'
+    className='h-full w-full pointer-events-none object-contain'
+  >
+    <path d='M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0' />
+    <path d='M12 7v5l3 3' />
+  </svg>
+)
+
+export const IconDownload = ({ strokeWidth = '1.5' }: { strokeWidth?: string }) => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    width='24'
+    height='24'
+    viewBox='0 0 24 24'
+    fill='none'
+    stroke='currentColor'
+    strokeWidth={strokeWidth}
+    strokeLinecap='round'
+    strokeLinejoin='round'
+    className='h-full w-full pointer-events-none object-contain'
+  >
+    <path d='M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4' />
+    <polyline points='7 10 12 15 17 10' />
+    <line x1='12' x2='12' y1='15' y2='3' />
+  </svg>
+)
+
+export const IconBookMark = ({ strokeWidth = '1.5' }: { strokeWidth?: string }) => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    width='24'
+    height='24'
+    viewBox='0 0 24 24'
+    fill='none'
+    stroke='currentColor'
+    strokeWidth={strokeWidth}
+    strokeLinecap='round'
+    strokeLinejoin='round'
+    className='h-full w-full pointer-events-none object-contain'
+  >
+    <path d='m19 21-7-4-7 4V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2v16z' />
+  </svg>
+)
+
+export const IconHeart = ({ strokeWidth = '1.5' }: { strokeWidth?: string }) => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    width='24'
+    height='24'
+    viewBox='0 0 24 24'
+    fill='none'
+    stroke='currentColor'
+    strokeWidth={strokeWidth}
+    strokeLinecap='round'
+    strokeLinejoin='round'
+    className='h-full w-full pointer-events-none object-contain'
+  >
+    <path d='M19 14c1.49-1.46 3-3.21 3-5.5A5.5 5.5 0 0 0 16.5 3c-1.76 0-3 .5-4.5 2-1.5-1.5-2.74-2-4.5-2A5.5 5.5 0 0 0 2 8.5c0 2.3 1.5 4.05 3 5.5l7 7Z' />
+  </svg>
+)
+
+export const IconHeadphone = ({ strokeWidth = '1.5' }: { strokeWidth?: string }) => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    width='24'
+    height='24'
+    viewBox='0 0 24 24'
+    fill='none'
+    stroke='currentColor'
+    strokeWidth={strokeWidth}
+    strokeLinecap='round'
+    strokeLinejoin='round'
+    className='h-full w-full pointer-events-none object-contain'
+  >
+    <path d='M3 14h3a2 2 0 0 1 2 2v3a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-7a9 9 0 0 1 18 0v7a2 2 0 0 1-2 2h-1a2 2 0 0 1-2-2v-3a2 2 0 0 1 2-2h3' />
+  </svg>
+)
+
+export const IconVideo = ({ strokeWidth = '1.5' }: { strokeWidth?: string }) => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    width='24'
+    height='24'
+    viewBox='0 0 24 24'
+    fill='none'
+    stroke='currentColor'
+    strokeWidth={strokeWidth}
+    strokeLinecap='round'
+    strokeLinejoin='round'
+    className='h-full w-full pointer-events-none object-contain'
+  >
+    <path d='m16 13 5.223 3.482a.5.5 0 0 0 .777-.416V7.87a.5.5 0 0 0-.752-.432L16 10.5' />
+    <rect x='2' y='6' width='14' height='12' rx='2' />
+  </svg>
+)
+
+export const IconMusic = ({ strokeWidth = '1.5' }: { strokeWidth?: string }) => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    width='24'
+    height='24'
+    viewBox='0 0 24 24'
+    fill='none'
+    stroke='currentColor'
+    strokeWidth={strokeWidth}
+    strokeLinecap='round'
+    strokeLinejoin='round'
+    className='h-full w-full pointer-events-none object-contain'
+  >
+    <path d='M9 18V5l12-2v13' />
+    <circle cx='6' cy='18' r='3' />
+    <circle cx='18' cy='16' r='3' />
+  </svg>
+)
+
+export const IconYoutube = ({ strokeWidth = '1.5' }: { strokeWidth?: string }) => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    width='24'
+    height='24'
+    viewBox='0 0 24 24'
+    fill='none'
+    stroke='currentColor'
+    strokeWidth={strokeWidth}
+    strokeLinecap='round'
+    strokeLinejoin='round'
+    className='h-full w-full pointer-events-none object-contain'
+  >
+    <path d='M2.5 17a24.12 24.12 0 0 1 0-10 2 2 0 0 1 1.4-1.4 49.56 49.56 0 0 1 16.2 0A2 2 0 0 1 21.5 7a24.12 24.12 0 0 1 0 10 2 2 0 0 1-1.4 1.4 49.55 49.55 0 0 1-16.2 0A2 2 0 0 1 2.5 17' />
+    <path d='m10 15 5-3-5-3z' />
+  </svg>
+)
+
+export const IconHardDrive = ({ strokeWidth = '1.5' }: { strokeWidth?: string }) => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    width='24'
+    height='24'
+    viewBox='0 0 24 24'
+    fill='none'
+    stroke='currentColor'
+    strokeWidth={strokeWidth}
+    strokeLinecap='round'
+    strokeLinejoin='round'
+    className='h-full w-full pointer-events-none object-contain'
+  >
+    <line x1='22' x2='2' y1='12' y2='12' />
+    <path d='M5.45 5.11 2 12v6a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2v-6l-3.45-6.89A2 2 0 0 0 16.76 4H7.24a2 2 0 0 0-1.79 1.11z' />
+    <line x1='6' x2='6.01' y1='16' y2='16' />
+    <line x1='10' x2='10.01' y1='16' y2='16' />
+  </svg>
+)
