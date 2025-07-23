d32b0e4a6e49ea477817395edf672471fd57d92e
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-07-22 22:49:43 -0300

    feat: agregar páginas a routes.ts

--- app/routes.ts
+++ app/routes.ts
@@ -2,5 +2,6 @@ import { type RouteConfig, index, route } from '@react-router/dev/routes'
 
 export default [
   index('routes/home.tsx'),
-  route('/watch', 'routes/watch.tsx')
+  route('/watch', 'routes/watch.tsx'),
+  route('/worker', 'routes/worker.tsx')
 ] satisfies RouteConfig
